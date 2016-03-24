ae8d86a38d4f9751ebaf38c1a8fc429b95e7b531
Adam Pfister <apfister@esri.com>
2016-03-24 12:08:10 -0500

    changed cedar chart ref to s3

--- app/components/area-line-chart/component.js
+++ app/components/area-line-chart/component.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement() {
-    const src = 'http://localhost:4300/area-line.json';
+    const src = 'https://s3.amazonaws.com/project-climate/cedar-types/area-line.json';
     let chart = new Cedar({specification: src});
     const sample_data =  {"features":[ { attributes: { "Date" : 1970, "Value" : 500, "Value2" : 550 } },{ attributes: { "Date" : 1980, "Value" : 550, "Value2" : 475 } },{ attributes: { "Date" : 1990, "Value" : 600, "Value2" : 370 } },{ attributes: { "Date" : 2000, "Value" : 650, "Value2" : 410 } },{ attributes: { "Date" : 2010, "Value" : 700, "Value2" : 645 } },{ attributes: { "Date" : 2020, "Value" : 800, "Value2" : 780 } } ]};
     let dataset = {
