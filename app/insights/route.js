c0671e3e9d2abcea1cf8d38a84a8dac079b9d83b
fgassert <cowbox314@gmail.com>
2016-05-24 11:33:09 -0400

    scroll to top; home imgs; partners page

--- app/insights/route.js
+++ app/insights/route.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  activate() {
+    this._super();
+    window.scrollTo(0,0);
+  },
   beforeModel(transition) {
     let svc = this.get('session');
     svc.set('headerSubStyle', 'header-data'); 
