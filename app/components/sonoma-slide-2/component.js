29c45d9cba453299ac4258287ee145a49fb2ac3a
Clara Linos <clara.linos@gmail.com>
2016-06-03 11:00:43 +0200

    fixed map

--- app/components/sonoma-slide-2/component.js
+++ app/components/sonoma-slide-2/component.js
@@ -15,23 +15,29 @@ export default Ember.Component.extend({
         this.removeMap();
       }
     }.bind(this));
-    this.initMap();
   },
 
   initMap(){
-    let mapOptions = {
+    const mapOptions = {
       zoomControl: false,
-      scrollWheelZoom:false
+      scrollWheelZoom:false,
+      center: [38.290957,-122.457728],
+      zoom: 10,
+      basemapSpec: {
+        url: 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw',
+        options: {
+          attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+          id: 'mapbox.streets',
+          maxZoom: 18
+        }
+      }
     };
+
     if (!this.slideMap){
-      this.slideMap = L.map('map', mapOptions).setView([38.290957,-122.457728], 8);
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-  			maxZoom: 18,
-  			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-  				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-  				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  			id: 'mapbox.streets'
-  		}).addTo(this.slideMap);
+      this.slideMap = L.map('map', mapOptions);
+      L.tileLayer(mapOptions.basemapSpec.url, mapOptions.basemapSpec.options).addTo(this.slideMap);
     }
   },
 
