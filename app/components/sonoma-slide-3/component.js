952ddbd2a3e1a5eedc5f451e91eece0bd0d13bda
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-06-07 13:44:06 +0200

    Adds the slide 3-1

--- app/components/sonoma-slide-3/component.js
+++ app/components/sonoma-slide-3/component.js
@@ -1,210 +1,347 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
   tagName: 'section',
-  classNames: ['sonoma-slide','slide-3'],
-  attributeBindings:["data-title"],
-  "data-title": 'Slide 3',
 
   vegaSpec: {
-    "padding": {"top": 25,"left": 45,"bottom": 30,"right": 20},
-    "width": 270,
-    "height": 240,
-    "data": [
-      {
-        "name": "table",
-        "values": [
-          {"x": "1990","y": 49882.21334},
-          {"x": "1991","y": 50439.99762},
-          {"x": "1992","y": 53788.76345},
-          {"x": "1993","y": 54091.51656},
-          {"x": "1994","y": 54430.268},
-          {"x": "1995","y": 55702.0407},
-          {"x": "1996","y": 57092.01688},
-          {"x": "1997","y": 57529.63805},
-          {"x": "1998","y": 57811.34489},
-          {"x": "1999","y": 57926.15725},
-          {"x": "2000","y": 59692.20263},
-          {"x": "2001","y": 61106.12063},
-          {"x": "2002","y": 62043.8042},
-          {"x": "2003","y": 64553.98158},
-          {"x": "2004","y": 66915.40318},
-          {"x": "2005","y": 68740.32298},
-          {"x": "2006","y": 69965.1020100001},
-          {"x": "2007","y": 71781.30638},
-          {"x": "2008","y": 72262.57875},
-          {"x": "2009","y": 69906.36943},
-          {"x": "2010","y": 73082.2306000001},
-          {"x": "2011","y": 74736.86963}
-        ],
-        "format": {"parse": {"x": "date"}}
-      },
-      {
-        "name": "summary",
-        "source": "table",
-        "transform": [
-          {
-            "type": "aggregate",
-            "summarize": {"y": ["min","max"]}
-          },
-          {
-            "type": "formula",
-            "field": "difference",
-            "expr": "datum.max_y-datum.min_y"
-          },
-          {
-            "type": "formula",
-            "field": "min",
-            "expr": "datum.min_y === 0 ? 0 : (datum.difference > 0 ? datum.min_y - datum.difference * 0.2  : datum.min_y * 0.8)"
-          },
-          {
-            "type": "formula",
-            "field": "min",
-            "expr": "datum.min < 0 ? 0 : datum.min"
-          },
-          {
-            "type": "formula",
-            "field": "max",
-            "expr": "datum.max_y === 0 ? 10 : (datum.difference > 0 ? datum.max_y + datum.difference * 0.2 : datum.max_y * 1.2)"
-          }
-        ]
-      },
-      {
-        "name": "computed",
-        "source": "table",
-        "transform": [{"type": "cross","with": "summary"}]
-      }
-    ],
-    "scales": [
-      {
-        "name": "x",
-        "type": "time",
-        "range": "width",
-        "domain": {"data": "table","field": "x"}
-      },
-      {
-        "name": "y",
-        "type": "linear",
-        "range": "height",
-        "domain": {"data": "computed","field": "a.y"},
-        "domainMin": {"data": "computed","field": "b.min"},
-        "domainMax": {"data": "computed","field": "b.max"},
-        "zero": false,
-        "nice": true
-      }
-    ],
-    "axes": [
-      {
-        "name": "lbl",
-        "type": "x",
-        "scale": "x",
-        "ticks": 5,
-        "format": "%Y",
-        "properties": {
-          "ticks": {"strokeWidth": {"value": 0}},
-          "axis": {
-            "stroke": {"value": "#333"},
-            "strokeWidth": {"value": 0}
-          },
-          "labels": {
-            "fontSize": {"value": 10},
-            "fontWeight": {"value": 300},
-            "fill": {"value": "#9BA2AA"},
-            "dx": {"value": 5}
-          }
-        }
-      },
-      {
-        "type": "y",
-        "ticks": 7,
-        "scale": "y",
-        "grid": true,
-        "layer": "back",
-        "format": "f",
-        "properties": {
-          "ticks": {"stroke": {"value": "steelblue"}},
-          "majorTicks": {"strokeWidth": {"value": 0}},
-          "axis": {
-            "stroke": {"value": "#333"},
-            "strokeWidth": {"value": 0}
-          },
-          "labels": {
-            "fontSize": {"value": 10},
-            "fontWeight": {"value": 300},
-            "fill": {"value": "#9BA2AA"}
-          }
-        }
-      }
-    ],
-    "marks": [
-      {
-        "type": "rect",
-        "from": {"data": "table"},
-        "properties": {
-          "enter": {
-            "x": {"scale": "x","field": "x"},
-            "width": {"field": {"group": "width"},"mult": 0.03},
-            "y": {"scale": "y","field": "y"},
-            "y2": {"field": {"group": "height"}}
-          },
-          "update": {"fill": {"value": "#5BB1D2"}},
-          "hover": {"fill": {"value": "#c32d7b"}}
-        }
-      },
-      {
-        "type": "text",
-        "from": {
-          "data": "table",
-          "transform": [{"type": "facet","groupby": ["unit"]}]
+  "padding": {"top": 30,"left": 25,"bottom": 50,"right": 20},
+  "width": 400,
+  "height": 240,
+  "data": [
+    {
+      "name": "bar-1",
+      "values": [
+        {"x": "1990","y": 27},
+        {"x": "1991","y": 25},
+        {"x": "1992","y": 30},
+        {"x": "1993","y": 23},
+        {"x": "1994","y": 25},
+        {"x": "1995","y": 25},
+        {"x": "1996","y": 24},
+        {"x": "1997","y": 22},
+        {"x": "1998","y": 21},
+        {"x": "1999","y": 20},
+        {"x": "2000","y": 24},
+        {"x": "2001","y": 26},
+        {"x": "2002","y": 30},
+        {"x": "2003","y": 32},
+        {"x": "2004","y": 31},
+        {"x": "2005","y": 30},
+        {"x": "2006","y": 31},
+        {"x": "2007","y": 29},
+        {"x": "2008","y": 28},
+        {"x": "2009","y": 30},
+        {"x": "2010","y": 30},
+        {"x": "2011","y": 32}
+      ],
+      "format": {"parse": {"x": "date"}}
+    },
+    {
+      "name": "bar-2",
+      "values": [
+        {"x": "1990","y": 22},
+        {"x": "1991","y": 20},
+        {"x": "1992","y": 24},
+        {"x": "1993","y": 17},
+        {"x": "1994","y": 21},
+        {"x": "1995","y": 21},
+        {"x": "1996","y": 23},
+        {"x": "1997","y": 17},
+        {"x": "1998","y": 17},
+        {"x": "1999","y": 18},
+        {"x": "2000","y": 19},
+        {"x": "2001","y": 22},
+        {"x": "2002","y": 25},
+        {"x": "2003","y": 23},
+        {"x": "2004","y": 24},
+        {"x": "2005","y": 18},
+        {"x": "2006","y": 20},
+        {"x": "2007","y": 21},
+        {"x": "2008","y": 23},
+        {"x": "2009","y": 24},
+        {"x": "2010","y": 24},
+        {"x": "2011","y": 26}
+      ],
+      "format": {"parse": {"x": "date"}}
+    },
+    {
+      "name": "axis",
+      "values": [{"x": "Year","y": "Number of days exceeding"}]
+    },
+    {
+      "name": "legend-1",
+      "values": [{"name": "> 95°F","color": "#efa600"}]
+    },
+    {
+      "name": "legend-2",
+      "values": [{"name": "> 100°F","color": "#1a3e62"}]
+    }
+  ],
+  "scales": [
+    {
+      "name": "x",
+      "type": "time",
+      "range": "width",
+      "domain": {"data": "bar-1","field": "x"}
+    },
+    {
+      "name": "y",
+      "type": "linear",
+      "range": "height",
+      "domain": {"data": "bar-1","field": "y"},
+      "nice": true
+    }
+  ],
+  "axes": [
+    {
+      "name": "lbl",
+      "type": "x",
+      "scale": "x",
+      "ticks": 5,
+      "format": "%Y",
+      "properties": {
+        "ticks": {"strokeWidth": {"value": 0}},
+        "axis": {
+          "stroke": {"value": "#3B4F63"},
+          "opacity": {"value": 0.5},
+          "strokeWidth": {"value": 0}
         },
-        "properties": {
-          "enter": {
-            "x": {"value": -30},
-            "y": {"value": -15},
-            "text": {"template": "{{datum.unit}}"},
-            "fontSize": {"value": 10},
-            "fontWeight": {"value": 300},
-            "fill": {"value": "#9BA2AA"},
-            "align": {"value": "left"}
-          }
+        "labels": {
+          "font": {"value": "\"Montserrat\", sans-serif"},
+          "fontSize": {"value": 10},
+          "fontWeight": {"value": 300},
+          "fill": {"value": "#3B4F63"},
+          "opacity": {"value": 0.5},
+          "dy": {"value": 5}
         }
       }
-    ]
-  },
-
-  didRender: function() {
-    Reveal.addEventListener('slidechanged', function( event ) {
-      if (event.currentSlide.classList.contains('slide-3')) {
-        this._iniChart();
+    },
+    {
+      "type": "y",
+      "ticks": 7,
+      "scale": "y",
+      "grid": true,
+      "layer": "back",
+      "format": "f",
+      "properties": {
+        "ticks": {"stroke": {"value": "steelblue"}},
+        "majorTicks": {"strokeWidth": {"value": 0}},
+        "axis": {
+          "stroke": {"value": "#333"},
+          "strokeWidth": {"value": 0}
+        },
+        "grid": {
+          "stroke": {"value": "#000"},
+          "strokeOpacity": {"value": 0.1},
+          "strokeWidth": {"value": 1}
+        },
+        "labels": {
+          "fontSize": {"value": 10},
+          "fontWeight": {"value": 300},
+          "fill": {"value": "#3B4F63"},
+          "opacity": {"value": 0.5}
+        }
       }
-    }.bind(this));
+    }
+  ],
+  "marks": [
+    {
+      "type": "rect",
+      "from": {"data": "bar-1"},
+      "properties": {
+        "enter": {
+          "x": {
+            "scale": "x",
+            "field": "x",
+            "mult": 0.98
+          },
+          "width": {"value": 4},
+          "y": {"scale": "y","field": "y"},
+          "y2": {"field": {"group": "height"}},
+          "fill": {"value": "#ffc94e"},
+          "strokeWidth": {"value": 0}
+        }
+      }
+    },
+    {
+      "type": "rect",
+      "from": {"data": "bar-2"},
+      "properties": {
+        "enter": {
+          "x": {
+            "scale": "x",
+            "field": "x",
+            "mult": 0.98,
+            "offset": 5
+          },
+          "width": {"value": 4},
+          "y": {"scale": "y","field": "y"},
+          "y2": {"field": {"group": "height"}},
+          "fill": {"value": "#263e57"},
+          "strokeWidth": {"value": 0}
+        }
+      }
+    },
+    {
+      "type": "text",
+      "from": {"data": "axis"},
+      "properties": {
+        "enter": {
+          "x": 0,
+          "y": 0,
+          "text": {"template": "{{datum.y | upper}}"},
+          "dx": {"value": -25},
+          "dy": {"value": -20},
+          "font": {"value": "\"Montserrat\", sans-serif"},
+          "fontSize": {"value": 10},
+          "fontWeight": {"value": 700},
+          "fill": {"value": "#3B4F63"},
+          "opacity": {"value": 0.5},
+          "align": {"value": "left"}
+        }
+      }
+    },
+    {
+      "type": "text",
+      "from": {"data": "axis"},
+      "properties": {
+        "enter": {
+          "x": 0,
+          "y": {"field": {"group": "height"},"mult": 1},
+          "text": {"template": "{{datum.x | upper}}"},
+          "dx": {"value": -25},
+          "dy": {"value": 22},
+          "font": {"value": "\"Montserrat\", sans-serif"},
+          "fontSize": {"value": 10},
+          "fontWeight": {"value": 700},
+          "fill": {"value": "#3B4F63"},
+          "opacity": {"value": 0.5},
+          "align": {"value": "left"}
+        }
+      }
+    },
+    {
+      "type": "rect",
+      "from": {"data": "legend-1"},
+      "properties": {
+        "enter": {
+          "x": {"value": -25},
+          "y": {
+            "field": {"group": "height"},
+            "mult": 1,
+            "offset": 45
+          },
+          "width": {"value": 9},
+          "y2": {
+            "field": {"group": "height"},
+            "mult": 1,
+            "offset": 48
+          },
+          "fill": {"field": "color"}
+        }
+      }
+    },
+    {
+      "type": "text",
+      "from": {"data": "legend-1"},
+      "properties": {
+        "enter": {
+          "x": 0,
+          "y": {"field": {"group": "height"},"mult": 1},
+          "text": {"template": "{{datum.name | upper}}"},
+          "dx": {"value": -9},
+          "dy": {"value": 50},
+          "font": {"value": "\"Montserrat\", sans-serif"},
+          "fontSize": {"value": 10},
+          "fontWeight": {"value": 700},
+          "fill": {"value": "#3b4f63"},
+          "opacity": {"value": 0.7},
+          "align": {"value": "left"}
+        }
+      }
+    },
+    {
+      "type": "rect",
+      "from": {"data": "legend-2"},
+      "properties": {
+        "enter": {
+          "x": {
+            "value": 40
+          },
+          "y": {
+            "field": {"group": "height"},
+            "mult": 1,
+            "offset": 45
+          },
+          "width": {"value": 9},
+          "y2": {
+            "field": {"group": "height"},
+            "mult": 1,
+            "offset": 48
+          },
+          "fill": {"field": "color"}
+        }
+      }
+    },
+    {
+      "type": "text",
+      "from": {"data": "legend-2"},
+      "properties": {
+        "enter": {
+          "x": {
+            "value": 40
+          },
+          "y": {"field": {"group": "height"},"mult": 1},
+          "text": {"template": "{{datum.name | upper}}"},
+          "dx": {"value": 16},
+          "dy": {"value": 50},
+          "font": {"value": "\"Montserrat\", sans-serif"},
+          "fontSize": {"value": 10},
+          "fontWeight": {"value": 700},
+          "fill": {"value": "#3b4f63"},
+          "opacity": {"value": 0.7},
+          "align": {"value": "left"}
+        }
+      }
+    }
+  ]
+},
 
-    this.el = this.$('#chart3-1');
-
-    this._iniChart();
+  didRender() {
+    this.$chart = this.$('#chart3-1');
+    this.initChart();
     this.setListeners();
   },
 
   setListeners: function() {
     this.refreshEvent = _.debounce(_.bind(this.update, this), 300);
-    $(window).on('resize', _.bind(this.update, this));
+    $(window).on('resize', this.refreshEvent);
   },
 
-  /**
-   * Method to redraw chart
-   */
   update: function() {
     if (this.chart) {
       this.chart.destroy();
       this.chart = null;
-      this._iniChart();
+      this.initChart();
     }
   },
 
-  /**
-   * Method to override and parese vega JSON parameterization
-   * @return {Object}
-   */
+  getSize: function() {
+    const vegaSpec = this.vegaSpec;
+    const widthSpace = vegaSpec.padding ?
+      vegaSpec.padding.left + vegaSpec.padding.right : 0;
+    const heightSpace = vegaSpec.padding ?
+      vegaSpec.padding.top + vegaSpec.padding.bottom : 0;
+
+    const containerSize = this.$chart[0].getBoundingClientRect();
+
+    return {
+      width: containerSize.width - widthSpace,
+      height: containerSize.height - heightSpace
+    };
+  },
+
   getVegaSpec: function() {
     var size = this.getSize();
     this.vegaSpec.width = size.width;
@@ -212,27 +349,9 @@ export default Ember.Component.extend({
     return this.vegaSpec;
   },
 
-  /**
-   * Calculate width and height
-   * @return {Object}
-   */
-  getSize: function() {
-    var vegaSpec = this.vegaSpec;
-    var widthSpace = vegaSpec.padding ?
-      vegaSpec.padding.left + vegaSpec.padding.right : 0;
-    var heightSpace = vegaSpec.padding ?
-      vegaSpec.padding.top + vegaSpec.padding.bottom : 0;
-    return {
-      width: this.el.clientWidth - widthSpace,
-      height: this.el.clientHeight - heightSpace
-    };
-  },
-
-  _iniChart: function() {
+  initChart: function() {
     var vegaSpec = this.getVegaSpec();
-    vg.parse.spec(vegaSpec, function(chart) {
-      chart({el:"#chart3-1"})
-        .update();
-    });
+    vg.parse.spec(vegaSpec, chart => chart({ el: this.$chart[0] }).update());
   }
+
 });
