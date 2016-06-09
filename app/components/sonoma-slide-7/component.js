1c66582ffa77567beaf1781be9648deb10462488
j8seangel <jseangel@gmail.com>
2016-06-09 19:02:27 +0200

    Add raster map and fit bounds

--- app/components/sonoma-slide-7/component.js
+++ app/components/sonoma-slide-7/component.js
@@ -4,6 +4,8 @@ export default Ember.Component.extend({
 
   tagName: 'section',
 
+  cartodbtable: 'cwd1981_2010_ave_hst_1415990012',
+
   didRender() {
     this.slideMapEl = this.$('#map7-2');
     this.setListeners();
@@ -33,6 +35,34 @@ export default Ember.Component.extend({
         }
       }
     };
+
+    if (!this.slideMap){
+      this.slideMap = L.map('map7-2', mapOptions);
+      L.tileLayer(mapOptions.basemapSpec.url, mapOptions.basemapSpec.options).addTo(this.slideMap);
+      this.addRaster();
+      if (this.bounds){
+        this.fitBounds();
+      } else {
+        this.setBounds().done(function(data){
+          let coordinates = JSON.parse(data.rows[0].bbox).coordinates[0];
+          let southWest = L.latLng(coordinates[0][1],coordinates[0][0]),
+          northEast = L.latLng(coordinates[2][1],coordinates[2][0]);
+          this.bounds = L.latLngBounds(southWest, northEast);
+          this.fitBounds();
+        }.bind(this));
+      }
+    }
+  },
+
+  fitBounds: function(){
+    this.slideMap.fitBounds(this.bounds,{padding:[15,15]});
+  },
+  setBounds: function(){
+    let query  = 'SELECT ST_AsGeoJSON(ST_Envelope(ST_Union(ST_Transform(ST_Envelope(the_raster_webmercator), 4326)))) as bbox FROM '+ this.cartodbtable +' as t';
+    return $.get('https://prep-admin.cartodb.com/api/v2/sql/?q='+query);
+  },
+
+  addRaster(){
     var request = {
       layers: [{
         "user_name": "prep-admin",
@@ -47,25 +77,22 @@ export default Ember.Component.extend({
         }
       }]
     };
-    if (!this.slideMap){
-      this.slideMap = L.map('map7-2', mapOptions);
-      L.tileLayer(mapOptions.basemapSpec.url, mapOptions.basemapSpec.options).addTo(this.slideMap);
-      $.ajax({
-        type: 'POST',
-        dataType: 'json',
-        contentType: 'application/json; charset=UTF-8',
-        url: 'https://prep-admin.cartodb.com/api/v1/map/',
-        data: JSON.stringify(request),
-        success: function(data) {
-          var tileUrl = 'https://prep-admin.cartodb.com/api/v1/map/' + data.layergroupid + '/{z}/{x}/{y}.png';
-          var layer = L.tileLayer(tileUrl);
-          layer.on('load',function(){
-            this.slideMapEl.removeClass('-loading');
-          }.bind(this));
-          layer.addTo(this.slideMap, 1);
-        }.bind(this)
-      });
-    }
+
+    $.ajax({
+      type: 'POST',
+      dataType: 'json',
+      contentType: 'application/json; charset=UTF-8',
+      url: 'https://prep-admin.cartodb.com/api/v1/map/',
+      data: JSON.stringify(request),
+      success: function(data) {
+        var tileUrl = 'https://prep-admin.cartodb.com/api/v1/map/' + data.layergroupid + '/{z}/{x}/{y}.png';
+        var layer = L.tileLayer(tileUrl);
+        layer.on('load',function(){
+          this.slideMapEl.removeClass('-loading');
+        }.bind(this));
+        layer.addTo(this.slideMap, 1);
+      }.bind(this)
+    });
   },
 
   removeMap(){
