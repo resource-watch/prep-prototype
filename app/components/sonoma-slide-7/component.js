8a1b30115c8d3c3878822d2f0bed0167cb49b53f
j8seangel <jseangel@gmail.com>
2016-06-14 16:44:48 +0200

    Fix onLoad event and change flow grahp type

--- app/components/sonoma-slide-7/component.js
+++ app/components/sonoma-slide-7/component.js
@@ -89,13 +89,15 @@ export default Ember.Component.extend({
       data: JSON.stringify(request),
       success: function(data) {
         var tileUrl = 'https://prep-admin.cartodb.com/api/v1/map/' + data.layergroupid + '/{z}/{x}/{y}.png';
-        if (this.layer) {
+        if (this.slideMap && this.layer) {
           this.slideMap.removeLayer(this.layer);
           this.layer = null;
         }
         this.layer = L.tileLayer(tileUrl).addTo(this.slideMap, 1);
         this.layer.on('load',function() {
-          this.slideMapEl.removeClass('-loading');
+          if(this.slideMapEl){
+            this.slideMapEl.removeClass('-loading');
+          }
         }.bind(this));
       }.bind(this)
     });
